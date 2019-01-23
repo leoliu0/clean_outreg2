aa13782a7e7e517339bfcdccc28c1518388e1cec
leoliu0 <38640768+leoliu0@users.noreply.github.com>
2019-01-23 15:43:54 +1100

    Add files via upload

---
+++ clean_outreg2.py
@@ -0,0 +1,85 @@
+import sys, getopt
+import xml.etree.ElementTree as ET
+from glob import glob
+import csv
+import os
+
+def proc_file(f):
+    tree = ET.parse(f)
+    root = tree.getroot()
+    global hline_count
+    hline_count = False
+    with open(os.path.join(output_dir,f.replace('.xml','.tex')),'w',newline='') as wf:
+        wf.write('\hline\\\\ \n')
+        for x in root:
+            if x.attrib:
+                for y in x:
+                    for k,v in y.attrib.items():
+                        if 'ExpandedColumnCount' in k:
+                            global cols
+                            cols = int(v)                
+                    for z in y:
+                        if 'Row' in z.tag:
+                            if list(z) and len(list(z))==cols:
+                                newrow = proc_row(z.findall('.//{urn:schemas-microsoft-com:office:spreadsheet}Data'))
+                                if newrow:
+                                    wf.write(newrow + ' \n')
+        wf.write('\hline\\\\ \n')
+
+
+def proc_row(row):
+    row = [e.text for e in row]
+    none_count = 0
+    dot_count = 0
+    cells = []
+    for cell in row:
+        if cell == 'VARIABLES':
+            cell = ''
+        if cell is None:
+            none_count += 1
+            cell = ''
+        else:
+            if (cell == '(.)') or (cell == '.'):
+                dot_count += 1
+                cell = ''
+        cells.append(cell)
+    if cols == none_count:
+        global hline_count
+        if hline_count is False:
+            hline_count = True
+            return '\hline\\\\'+ '& '*(cols-1) + '\\\\'.strip('"')
+        else:
+            return '& '*(cols-1) + '\\\\'
+    if cols - (none_count + dot_count) <= 1:
+        return
+    return '& '.join(cells) + '\\\\'    
+
+def main(argv):
+    inputfiles = ''
+    vardict = ''
+    global output_dir
+    output_dir = ''
+    try:
+        opts, args = getopt.getopt(argv,"hi:o:")
+    except getopt.GetoptError:
+        print ('clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print ('clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>')
+        elif opt == '-i':
+            inputfiles = arg
+        elif opt == '-d':
+            vardict = arg
+        elif opt == '-o':
+            output_dir = arg
+    if ',' in inputfiles:
+        files = inputfiles.split(',')
+    else:
+        files = glob(inputfiles)
+    
+    for f in files:
+        proc_file(f)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
