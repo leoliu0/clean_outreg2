bc12c5ea94fdc1ee7cf5dd51f69c23abe1548de7
Leo Liu <auleo.liu@gmail.com>
2020-03-20 15:49:06 +1100

    improve py

--- clean_outreg2.py
+++ clean_outreg2.py
@@ -5,10 +5,13 @@ import sys
 import xml.etree.ElementTree as ET
 from glob import glob
 
-print ('USAGE: python clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>')
+print(
+    'USAGE: python clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>'
+)
+
 
 def proc_file(f):
-    print('-----processing ',f)
+    print('-----processing ', f)
     if '.xml' not in f:
         print('----------this is not xml file, skipped')
         return
@@ -16,23 +19,27 @@ def proc_file(f):
     root = tree.getroot()
     global hline_count
     hline_count = False
-    output_file = os.path.join(output_dir.replace('"','').replace("'",''),
-                                        os.path.split(f)[-1].replace('.xml','.tex'))
+    output_file = os.path.join(
+        output_dir.replace('"', '').replace("'", ''),
+        os.path.split(f)[-1].replace('.xml', '.tex'))
 
-    with open(output_file,'w',newline='') as wf:
+    with open(output_file, 'w', newline='') as wf:
         print(output_file, ' Saved')
         for x in root:
             if x.attrib:
                 for y in x:
-                    for k,v in y.attrib.items():
+                    for k, v in y.attrib.items():
                         if 'ExpandedColumnCount' in k:
                             global cols
                             cols = int(v)
-                    wf.write('& '*(cols - 1) + '\\\\ \\hline')
+                    wf.write('& ' * (cols - 1) + '\\\\ \\hline')
                     for z in y:
                         if 'Row' in z.tag:
-                            if list(z) and len(list(z))==cols:
-                                newrow = proc_row(z.findall('.//{urn:schemas-microsoft-com:office:spreadsheet}Data'))
+                            if list(z) and len(list(z)) == cols:
+                                newrow = proc_row(
+                                    z.findall(
+                                        './/{urn:schemas-microsoft-com:office:spreadsheet}Data'
+                                    ))
                                 if newrow:
                                     wf.write(newrow + ' \n')
         wf.write('\hline\\\\ \n')
@@ -43,9 +50,9 @@ def proc_row(row):
     none_count = 0
     dot_count = 0
     cells = []
-    for n,cell in enumerate(row):
-        if n==0:
-            none_count += 1
+    for n, cell in enumerate(row):
+        # if n == 0:
+        # none_count += 1
         if cell == 'VARIABLES':
             cell = ''
         if cell is None:
@@ -56,37 +63,44 @@ def proc_row(row):
                 dot_count += 1
                 cell = ''
             if vardict:
-                if cell in vardict:
-                    cell = vardict[cell]
-        cell = cell.replace('#','$\\times$').replace('_',' ')
+                if cell.strip() in vardict:
+                    cell = vardict[cell.strip()]
+        cell = cell.replace('#', '$\\times$').replace('_', ' ')
         cells.append(cell)
+    if set(cells[1:]) == {''} and cells[0] != '':
+        return '\\\\'
     if cols == none_count:
         global hline_count
         if hline_count is False:
             hline_count = True
-            return '\hline\\\\'+ '& '*(cols-1) + '\\\\'.strip('"')
+            return '\hline\\\\' + '& ' * (cols - 1) + '\\\\'.strip('"')
         else:
-            return '& '*(cols-1) + '\\\\'
+            return '& ' * (cols - 1) + '\\\\'
     if cols - (none_count + dot_count) == 0:
         return
     return '& '.join(cells) + '\\\\'
 
+
 def main(argv):
     global vardict
     global output_dir
-    inputfiles,vardict,output_dir = '','',''
+    inputfiles, vardict, output_dir = '', '', ''
     try:
-        opts, args = getopt.getopt(argv,"hi:d:o:")
+        opts, args = getopt.getopt(argv, "hi:d:o:")
         print(opts)
     except getopt.GetoptError:
-        print ('clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>')
+        print(
+            'clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>'
+        )
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print ('clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>')
+            print(
+                'clean_outreg2.py -i <inputfile(s)> -d(optional) <var dict> -o(optional) <output directory>'
+            )
         elif opt == '-i':
             print(type(arg))
-            print('------'+arg)
+            print('------' + arg)
             inputfiles = arg
         elif opt == '-d':
             vardict = arg
@@ -98,11 +112,12 @@ def main(argv):
         files = glob(inputfiles)
 
     if vardict:
-        with open(vardict,'r') as f:
+        with open(vardict, 'r') as f:
             vardict = json.load(f)
     print(f'processing {len(files)} files')
     for f in files:
         proc_file(f)
 
+
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    main(sys.argv[1:])
